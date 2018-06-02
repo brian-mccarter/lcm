183fecb4f22118a472365be84efdd49be50526e6
Tim Perkins <tprk77@gmail.com>
2018-06-02 13:36:24 -0400

    Use Perl for MacOS script dir

--- format_code.sh
+++ format_code.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+if (perl -e '' 2>/dev/null)
+  then SCRIPT_DIR="$(dirname "$(perl -MCwd -le 'print Cwd::abs_path(shift)' "$0")")"
+  else SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+fi
+
+export SCRIPT_DIR
 
 function format_dir_r {
     find "$1" -regex '.*\.\(c\|h\)\(pp\)?' -print0 | xargs -0 clang-format -i
